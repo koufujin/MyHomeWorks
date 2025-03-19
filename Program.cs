679089d44f05f8739b7c26f5d5f22ba37f658c44
Alexey Krasnyakov <alexfront1@mail.ru>
2025-03-19 21:52:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyHomeWorks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здесь все домашки");
+        }
+    }
+}
